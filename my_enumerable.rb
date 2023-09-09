b01dbf6367eaaad8a53507c2e2c91f4402b5b420
Chandan-devs-tech <chandanguptabsb@gmail.com>
2023-09-09 17:03:54 +0530

    Update my_enumerable.rb file

--- my_enumerable.rb
+++ my_enumerable.rb
@@ -1,13 +1,13 @@
 module MyEnumerbale
-    def all?(&block)
-        @list.all?(&block)
-    end
+  def all?(&block)
+    @list.all?(&block)
+  end
 
-    def any?(&block)
-        @list.any?(&block)
-    end
+  def any?(&block)
+    @list.any?(&block)
+  end
 
-    def filter(&block)
-        @list.select(&block)
-    end
+  def filter(&block)
+    @list.select(&block)
+  end
 end
