45fa7d71cf494698065efd89af391f7849ff4ba2
Chandan-devs-tech <chandanguptabsb@gmail.com>
2023-09-09 17:03:23 +0530

    Update main.rb file

---
+++ main.rb
@@ -0,0 +1,14 @@
+require_relative 'my_list'
+
+list = MyList.new(1, 2, 3, 4)
+
+# Test #all?
+puts(list.all? { |e| e < 5 }) # true
+puts(list.all? { |e| e > 5 }) # false
+
+# Test #any?
+puts(list.any? { |e| e == 2 }) # true
+puts(list.any? { |e| e == 5 }) # false
+
+# Test #filter
+puts(list.filter(&:even?)) # [2, 4]
